23db309eed746ac50b5ae292aa607ff47f9ac15f
C.J. Tran <2cj.tran@gmail.com>
2021-11-18 12:20:36 -0500

    Trying non-static instead

--- RAD_v4/RAD_v4/DBConnector.cs
+++ RAD_v4/RAD_v4/DBConnector.cs
@@ -10,43 +10,64 @@ namespace Controller
 {
     static class DBConnector
     {
-        static readonly SQLiteConnection conn = new SQLiteConnection();
-        static readonly SQLiteCommand cmd = new SQLiteCommand();
+        //static readonly SQLiteConnection conn = new SQLiteConnection();
+        //static readonly SQLiteCommand cmd = new SQLiteCommand();
 
         public static void Initialize()
         {
             try
             {
+                SQLiteConnection conn = new SQLiteConnection();
+                SQLiteCommand cmd = new SQLiteCommand();
                 // start DB
                 conn.Open();
                 //We might not want to have a drop schema line since that will wipe the User table every time the DB is initialized
                 // the 'typeof' for TYPE can be int for simplicity (since the enum occurs in local code)
-                cmd.CommandText = "" +
-                    "DROP SCHEMA (IF EXISTS)" +
-                    "CREATE TABLE User {" +
-                    "UName VARCHAR(50) PRIMARY KEY," +
-                    "PWord CHAR(16) PRIMARY KEY," +
-                    "TYPE ENUM (\"Customer\", \"Admin\")" +
-                    "}";
-                cmd.ExecuteNonQuery();
+
+                //cmd.CommandText = "" +
+                //    "DROP TABLE [IF EXISTS] User;" +
+                //    "DROP TABLE [IF EXISTS] Keys;" +
+                //    "DROP TABLE [IF EXISTS] Log;";
+                //cmd.ExecuteNonQuery();
+
 
                 cmd.CommandText = "" +
-                    "CREATE TABLE Keys {" +
+                    "CREATE TABLE User (" +
+                    "UName VARCHAR(50) PRIMARY KEY," +
+                    "PWord CHAR(16) PRIMARY KEY," +
+                    "TYPE SMALLINT (\"Customer\", \"Admin\")" +
+                    ")";
+                cmd.ExecuteReader();
+
+                cmd.CommandText = "" +
+                    "CREATE TABLE Keys (" +
                     "ID VARCHAR(16) PRIMARY KEY," +
                     "Status VARCHAR(10)," +
                     "CurrentUser VARCHAR(16)," +
                     "PreviousUser VARCHAR(16)," +
                     "RoomNum SMALLINT" +
-                    "}";
-                cmd.ExecuteNonQuery();
+                    ")";
+                cmd.ExecuteReader();
 
                 cmd.CommandText = "" +
-                    "CREATE TABLE Log {" +
+                    "CREATE TABLE Log (" +
                     "User VARCHAR(16) PRIMARY KEY," +
                     "Login DATETIME," +
                     "Logout DATETIME" +
-                    "}";
+                    ")";
+                cmd.ExecuteReader();
+
+                SQLiteDataReader reader;
+                cmd.CommandText = "" +
+                    "SELECT * FROM User;";
                 cmd.ExecuteNonQuery();
+                reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    string myreader = reader.GetString(0);
+                    Console.WriteLine(myreader);
+                }
+                conn.Close();
             }
             catch (Exception)
             {
@@ -69,6 +90,9 @@ namespace Controller
         /* Updates a key's status via reservation */
         public static bool Save(Reservation res)
         {
+            SQLiteConnection conn = new SQLiteConnection();
+            SQLiteCommand cmd = new SQLiteCommand();
+
             cmd.CommandText = "" +
                 "UPDATE Keys" +
                 "Set KeyStatus = \"Pending\"" +
@@ -87,6 +111,9 @@ namespace Controller
         /* KeyStatus Getter */
         public static KeyStatus GetStatus(int key)
         {
+            SQLiteConnection conn = new SQLiteConnection();
+            SQLiteCommand cmd = new SQLiteCommand();
+
             cmd.CommandText = "" +
                 "SELECT Status" +
                 "FROM Keys" +
@@ -109,6 +136,9 @@ namespace Controller
         /* Updates Key's Status */
         public static void Save(KeyStatus keyStat)
         {
+            SQLiteConnection conn = new SQLiteConnection();
+            SQLiteCommand cmd = new SQLiteCommand();
+
             // save to DB
             cmd.CommandText = "" +
                 "UPDATE Keys" +
@@ -119,6 +149,9 @@ namespace Controller
         /* Saves logout info when user logs out */
         public static void SaveLogout(string name)
         {
+            SQLiteConnection conn = new SQLiteConnection();
+            SQLiteCommand cmd = new SQLiteCommand();
+
             // save to DB
             cmd.CommandText = "" +
                 "UPDATE Logs" +
