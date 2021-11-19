08c62bc3a5a02638df52cfa2e97ef75ae3713fcf
CJ Tran <CJTRAN@augusta.edu>
2021-11-19 12:57:00 -0500

    Working on Login UI

---
+++ RAD_v4/RAD_v4/Login.Designer.cs
@@ -0,0 +1,115 @@
+﻿
+namespace RAD_v4
+{
+    partial class Login
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.UsernameTextbox = new System.Windows.Forms.TextBox();
+            this.PasswordTextbox = new System.Windows.Forms.TextBox();
+            this.SignInButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(254, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "KRTS";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(205, 134);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(189, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Please Sign In:";
+            // 
+            // UsernameTextbox
+            // 
+            this.UsernameTextbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UsernameTextbox.Location = new System.Drawing.Point(210, 207);
+            this.UsernameTextbox.Name = "UsernameTextbox";
+            this.UsernameTextbox.Size = new System.Drawing.Size(184, 29);
+            this.UsernameTextbox.TabIndex = 2;
+            this.UsernameTextbox.Text = "Username";
+            // 
+            // PasswordTextbox
+            // 
+            this.PasswordTextbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PasswordTextbox.Location = new System.Drawing.Point(210, 268);
+            this.PasswordTextbox.Name = "PasswordTextbox";
+            this.PasswordTextbox.Size = new System.Drawing.Size(184, 29);
+            this.PasswordTextbox.TabIndex = 3;
+            this.PasswordTextbox.Text = "Password";
+            // 
+            // SignInButton
+            // 
+            this.SignInButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.SignInButton.FlatAppearance.BorderSize = 10;
+            this.SignInButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Black;
+            this.SignInButton.Location = new System.Drawing.Point(260, 356);
+            this.SignInButton.Name = "SignInButton";
+            this.SignInButton.Size = new System.Drawing.Size(85, 23);
+            this.SignInButton.TabIndex = 4;
+            this.SignInButton.Text = "Sign In";
+            this.SignInButton.UseVisualStyleBackColor = true;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Controls.Add(this.SignInButton);
+            this.Controls.Add(this.PasswordTextbox);
+            this.Controls.Add(this.UsernameTextbox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Login";
+            this.Size = new System.Drawing.Size(613, 485);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox UsernameTextbox;
+        private System.Windows.Forms.TextBox PasswordTextbox;
+        private System.Windows.Forms.Button SignInButton;
+    }
+}
