d0e6a647ce28a0c5f8109828fc887dae2f2373aa
Chris Judkins <chrisjudk@protonmail.com>
2021-11-19 20:24:27 -0500

    Initialized DB tables
    -User
    -Key
    -AccessEvent

---
+++ RAD_v4/KEY.sql
@@ -0,0 +1,9 @@
+﻿-- Script Date: 2021-11-19 20:06  - ErikEJ.SqlCeScripting version 3.5.2.90
+CREATE TABLE [Key] (
+  [Id] INTEGER NOT NULL
+, [CurrentAssigned] TEXT NULL
+, [LastAssigned] TEXT NULL
+, [RoomNum] INTEGER NOT NULL
+, [Status] INTEGER NOT NULL
+, CONSTRAINT [PK_Key] PRIMARY KEY ([Id])
+);
