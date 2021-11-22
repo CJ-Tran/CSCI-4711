9bbc575b8c98b5186a80e606e3fba9201c66ea3d
CJ Tran <CJTRAN@augusta.edu>
2021-11-21 21:19:38 -0500

    Fixed ManageKey?

--- RAD_v4/RAD_v4/ManageKeyForm.cs
+++ RAD_v4/RAD_v4/ManageKeyForm.cs
@@ -23,9 +23,9 @@ namespace Boundary
         //ManageControl mc;
         private User cUser;
 
-        public ManageKeyForm(User u ,Entity.KeyList kList)
+        public ManageKeyForm(User u, Entity.KeyList kList)
         {
-            
+
             cUser = u;
             InitializeComponent();
             AddKeys(kList);
@@ -46,10 +46,10 @@ namespace Boundary
             Display(ManageControl.GetStatus(k));
         }
 
-        public void Save(int index,StatusType status)
+        public void Save(int index, StatusType status)
         {
 
-            KeyStatus k = new KeyStatus(index,status);
+            KeyStatus k = new KeyStatus(index, status);
             ManageControl.Update(k);
             Display(k); // call KeyList_SelectedIndexChanged() better?
         }
@@ -76,14 +76,13 @@ namespace Boundary
             // KeyStatus
             // 
             this.KeyStatus.FormattingEnabled = true;
-            this.KeyStatus.ItemHeight = 16;
             this.KeyStatus.Items.AddRange(new object[] {
             "Available",
             "Pending",
             "Assigned"});
             this.KeyStatus.Location = new System.Drawing.Point(72, 277);
             this.KeyStatus.Name = "KeyStatus";
-            this.KeyStatus.Size = new System.Drawing.Size(120, 84);
+            this.KeyStatus.Size = new System.Drawing.Size(120, 82);
             this.KeyStatus.TabIndex = 1;
             this.KeyStatus.SelectedIndexChanged += new System.EventHandler(this.KeyStatus_SelectedIndexChanged);
             // 
@@ -92,7 +91,7 @@ namespace Boundary
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(72, 254);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(80, 17);
+            this.label1.Size = new System.Drawing.Size(61, 13);
             this.label1.TabIndex = 2;
             this.label1.Text = "Key Status:";
             // 
@@ -101,7 +100,7 @@ namespace Boundary
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(72, 58);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(79, 17);
+            this.label2.Size = new System.Drawing.Size(61, 13);
             this.label2.TabIndex = 3;
             this.label2.Text = "Select Key:";
             // 
@@ -110,7 +109,7 @@ namespace Boundary
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(348, 58);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(79, 17);
+            this.label3.Size = new System.Drawing.Size(60, 13);
             this.label3.TabIndex = 4;
             this.label3.Text = "Key Details";
             // 
@@ -119,9 +118,10 @@ namespace Boundary
             this.label4.AutoSize = true;
             this.label4.Location = new System.Drawing.Point(348, 81);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(46, 17);
+            this.label4.Size = new System.Drawing.Size(35, 13);
             this.label4.TabIndex = 5;
             this.label4.Text = "label4";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
             // button1
             // 
@@ -146,13 +146,13 @@ namespace Boundary
             // KeysList
             // 
             this.KeysList.FormattingEnabled = true;
-            this.KeysList.ItemHeight = 16;
             this.KeysList.Items.AddRange(new object[] {
             "roomKey"});
             this.KeysList.Location = new System.Drawing.Point(75, 81);
             this.KeysList.Name = "KeysList";
-            this.KeysList.Size = new System.Drawing.Size(120, 84);
+            this.KeysList.Size = new System.Drawing.Size(120, 82);
             this.KeysList.TabIndex = 8;
+            this.KeysList.SelectedIndexChanged += new System.EventHandler(this.KeysList_SelectedIndexChanged_1);
             // 
             // ManageKeyForm
             // 
@@ -186,17 +186,28 @@ namespace Boundary
 
         private void KeyStatus_SelectedIndexChanged(object sender, EventArgs e)
         {
-            
+
         }
 
         private void KeysList_SelectedIndexChanged(object sender, EventArgs e)
         {
-            label4.Text = KeysList.SelectedItem.ToString();//might need to override; not sure if this would display properl";
+
         }
 
         private void ManageKeyForm_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void KeysList_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            label4.Text = KeysList.SelectedIndex.ToString();//might need to override; not sure if this would display properl";
+            Refresh();
         }
     }
 }
