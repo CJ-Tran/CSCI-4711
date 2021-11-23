6f386616612fb5b0281b03b795252e08e0099e16
CJ Tran <CJTRAN@augusta.edu>
2021-11-22 21:04:25 -0500

    Deleted KRTS class & put its code in Program.Main()

--- RAD_v4/RAD_v4/Program.cs
+++ RAD_v4/RAD_v4/Program.cs
@@ -9,12 +9,14 @@ namespace RAD_v4
     /*
      * How I changed program from Console to Windows Application:
      * https://forum.openframeworks.cc/t/how-to-hide-a-console-window-when-launching-exe-file/28616
+     * 
+     * Deleted KRTS class and just put the StartupController.Initiate() in Program.Main()
      */
     class Program
     {
         static void Main(string[] args)
         {
-            KRTS.Run();
+            Controller.StartupController.Initiate();
         }
     }
 }
